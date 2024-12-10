1635e722672bde6d38f06ffabafa662c3b3ca1d6
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-12-10 19:16:44 +0000

    refactor(BooleanState): use the default state from the static property `state`, add stored `true` (#onTrueCallback) and `false`(#onFalseCallback) callback functions performed on state changes. Add `is()` `on()` `onFalse()` `onTrue()`  `setOnFalse()` `setOnTrue()` methods.

--- src/lib/boolean-state.abstract.ts
+++ src/lib/boolean-state.abstract.ts
@@ -10,59 +10,166 @@ import { State } from "./state.abstract";
  */
 export abstract class BooleanState extends State<boolean> {
   /**
+   * @description Default state for the instance.
+   * @public
+   * @static
+   * @type {boolean}
+   */
+  public static state = true;
+
+  /**
+   * @description Privately stored callback function performed on state set to `false`.
+   * @type {*}
+   */
+  #onFalseCallback;
+
+  /**
+   * @description Privately stored callback function performed on state set to `true`.
+   * @type {*}
+   */
+  #onTrueCallback;
+
+  /**
    * Creates an instance of parent class.
    * @constructor
-   * @param {?boolean} [state] Sets initially boolean state.
+   * @param {?boolean} [state] Sets initially `boolean` state.
+   * @param {?() => any} [onTrueCallback] Optional callback function performed on each state change to `true`.
+   * @param {?() => any} [onFalseCallback] Optional callback function performed on each state change to `false`.
    */
-  constructor(state: boolean = true) {
+  constructor(
+    state: boolean = BooleanState.state,
+    onTrueCallback?: () => any,
+    onFalseCallback?: () => any,
+  ) {
     super(state);
+    this.#onTrueCallback = onTrueCallback;
+    this.#onFalseCallback = onFalseCallback;
+    if (state) {
+      onTrueCallback?.();
+    } else {
+      onFalseCallback?.();
+    }
   }
 
   /**
    * @description Sets boolean state to `false`.
-   * @protected
+   * @public
+   * @param {*} [callback=this.#onFalseCallback] Performs the callback function if provided.
    * @returns {this}
    */
-  protected false(): this {
+  public false(callback = this.#onFalseCallback): this {
     super.set(false);
+    typeof callback === 'function' && callback();
     return this;
   }
 
   /**
-   * @description Checks whether state is `false`.
-   * @protected
+   * @description Checks the active state and perform callback.
+   * @public
+   * @param {?boolean} [expected] Expected state of `boolean` type.
+   * @param {?(result: boolean) => void} [callback] The callback function with a expected `result`.
    * @returns {boolean}
    */
-  protected isFalse() {
+  public is(expected?: boolean, callback?: (result: boolean) => void): boolean {
+    const result = typeof expected === 'boolean' ? this.state === expected : this.state;
+    callback?.(result);
+    return result;
+  }
+
+  /**
+   * @description Checks whether the state is `false`.
+   * @public
+   * @returns {boolean}
+   */
+  public isFalse() {
     return super.state === false;
   }
 
   /**
-   * @description Checks whether state is `true`.
-   * @protected
+   * @description Checks whether the state is `true`.
+   * @public
    * @returns {boolean}
    */
-  protected isTrue() {
-    return super.state === true;
+  public isTrue() {
+    return super.state === true
+  }
+
+  /**
+   * @description Performs the `callback` function on `state`.
+   * @public
+   * @param {(state: boolean) => void} stateCallback The callback function with a `state` to perform.
+   * @returns {this}
+   */
+  public on(stateCallback: (state: boolean) => void): this {
+    stateCallback(this.state);
+    return this;
+  }
+
+  /**
+   * @description Performs the `callback` function on state `false`.
+   * @public
+   * @param {() => void} callback The callback function to perform on state `false`.
+   * @returns {this}
+   */
+  public onFalse(callback: () => void): this {
+    this.isFalse() && callback();
+    return this;
+  }
+
+  /**
+   * @description Performs the `callback` function on state `true`.
+   * @public
+   * @param {() => void} callback The callback function to perform on state `true`.
+   * @returns {this}
+   */
+  public onTrue(callback: () => void): this {
+    this.isTrue() && callback();
+    return this;
+  }
+
+  /**
+   * @description Sets the callback function performed on state set to `false`.
+   * @public
+   * @param {() => void} onFalseCallback The callback function performed on state `false`.
+   * @returns {this}
+   */
+  public setOnFalse(onFalseCallback: () => void): this {
+    typeof onFalseCallback === 'function' && (this.#onFalseCallback = onFalseCallback);
+    return this;
+  }
+
+  /**
+   * @description Sets the callback function performed on state set to `true`.
+   * @public
+   * @param {() => void} onTrueCallback The callback function performed on state `true`.
+   * @returns {this}
+   */
+  public setOnTrue(onTrueCallback: () => void): this {
+    typeof onTrueCallback === 'function' && (this.#onTrueCallback = onTrueCallback);
+    return this;
   }
 
   /**
    * @description Toggle boolean state.
-   * @protected
+   * @public
    * @returns {this}
    */
-  protected toggle(): this {
-    super.set(!super.state)
+  public toggle(): this {
+    super.set(!super.state);
+    this.#onFalseCallback && this.onFalse(this.#onFalseCallback);
+    this.#onTrueCallback && this.onTrue(this.#onTrueCallback);
     return this;
   }
 
   /**
    * @description Sets boolean state to `true`.
-   * @protected
+   * @public
+   * @param {?() => void} [callback] Performs the callback function if provided.
    * @returns {this}
    */
-  protected true(): this {
+  public true(callback = this.#onTrueCallback): this {
     super.set(true);
+    typeof callback === 'function' && callback();
     return this;
   }
 }
