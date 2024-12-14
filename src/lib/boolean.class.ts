234acbaad2f80e79602688a6bf12cb63e9138750
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-12-14 00:30:34 +0000

    feat(Boolean): add non `abstract` class `Boolean` to import with the `as`.

---
+++ src/lib/boolean.class.ts
@@ -0,0 +1,10 @@
+// Abstract.
+import { BooleanState } from "./boolean-state.abstract";
+/**
+ * @description Handles the `boolean` type state. 
+ * @export
+ * @class Boolean
+ * @typedef {Boolean}
+ * @extends {BooleanState}
+ */
+export class Boolean extends BooleanState {}
