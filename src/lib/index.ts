934793ddddf6445c16b960d17cf5a3e6f6345a3d
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-12-15 15:02:57 +0000

    chore(index): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,43 +1,15 @@
-export {
-  Ability,
-  Disabled,
-  Enabled
-} from './ability';
-export {
-  Accessibility,
-  Accessible,
-  Inaccessible
-} from './accessibility';
-export {
-  Connected,
-  Connection,
-  Disconnected
-} from './connection';
-export {
-  Expandable,
-  Expanded,
-} from './expendable';
-export {
-  All,
-  Selectable,
-  Selected,
-} from './selectable';
-export {
-  Invisible,
-  Visibility,
-  Visible,
-} from './visibility';
-// Class state.
-export { Activated } from './activated.class';
-export { Active } from './active.class';
-export { Available } from './available.class';
-export { Deactivated } from './deactivated.class ';
-export { Edited } from './edited.class';
-export { Restricted } from './restricted.class';
-// Abstract state.
+// Boolean state.
+export { Ability } from './ability.class';
+export { Boolean } from './boolean.class';
+export { BooleanStates } from './boolean-states.class';
+// Enum state.
+export { Enum } from './enum.class';
+// Object abstract state.
+export { ImmutableState } from './immutable-state.abstract';
+// Data of type abstract state.
 export { BooleanState } from './boolean-state.abstract';
 export { EnumState } from './enum-state.abstract';
-export { ImmutableState } from './immutable-state.abstract';
 export { NullState } from './null-state.abstract';
 export { NumberState } from './number-state.abstract';
+// Common abstract state.
 export { State } from './state.abstract';
