64bb501f13b215257d9210439a40ca41327266cc
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-12-15 15:02:02 +0000

    docs(Boolean): remove `typedef`.

--- src/lib/boolean.class.ts
+++ src/lib/boolean.class.ts
@@ -1,10 +1,9 @@
 // Abstract.
 import { BooleanState } from "./boolean-state.abstract";
 /**
- * @description Handles the `boolean` type state. 
+ * @description Handles the `boolean` type state.
  * @export
  * @class Boolean
- * @typedef {Boolean}
  * @extends {BooleanState}
  */
 export class Boolean extends BooleanState {}
