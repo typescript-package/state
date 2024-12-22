e9d520f1cb05cbbc00fb4c8669ff08992ede26cf
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-12-22 01:07:33 +0000

    chore(index): update.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,10 @@
 // Boolean state.
 export { Boolean } from './boolean.class';
-export { BooleanStates } from './boolean-states.class';
+// Array state.
+export { ArrayState } from './array-state.abstract';
+export { BooleanArrayState } from './boolean-array-state.abstract';
+export { NamedArrayState } from './named-array-state.abstract';
+export { NamedBooleanArrayState } from './named-boolean-array-state.abstract';
 // Enum state.
 export { Enum } from './enum.class';
 // Object abstract state.
@@ -11,5 +15,6 @@ export { BooleanState } from './boolean-state.abstract';
 export { EnumState } from './enum-state.abstract';
 export { NullState } from './null-state.abstract';
 export { NumberState } from './number-state.abstract';
+export { ObjectState } from './object-state.abstract';
 // Common abstract state.
 export { State } from './state.abstract';
