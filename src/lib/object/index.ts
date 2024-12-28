e1b07873b1954fb94ee209664ec4dfd5dd3d9d98
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2024-12-28 02:16:43 +0000

    chore(index): update.

--- src/lib/object/index.ts
+++ src/lib/object/index.ts
@@ -1,3 +1,4 @@
-export { BooleanActionObjectState } from "./boolean-action-object-state.abstract";
-export { BooleanObjectState } from "./boolean-object-state.abstract";
-export { ObjectState } from "./object-state.abstract";
+export { BooleanActionObjectState } from './boolean-action-object-state.abstract';
+export { BooleanObjectState } from './boolean-object-state.abstract';
+export { NamedObjectState } from './named-object-state.abstract';
+export { ObjectState } from './object-state.abstract';
