a5aa10ab242bbe9758579390f273803194fa9371
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-19 11:46:18 +0000

    refactor(Selectable): use the `SelectablePrivate` by default, cause of the private fields approach.

--- src/lib/selectable/selectable.abstract.ts
+++ src/lib/selectable/selectable.abstract.ts
@@ -1,55 +1,20 @@
-// Class.
-import { Ability } from '../ability/ability.abstract';
-import { Boolean as Selected } from '../boolean';
+// Abstract.
+import { SelectablePrivate } from "./selectable-private.abstract";
 /**
- * @description Manages the selected state.
+ * @description The default `Selectable` that uses a modern approach private fields.
  * @export
+ * @abstract
  * @class Selectable
- * @extends {Ability}
+ * @extends {SelectablePrivate}
  */
-export abstract class Selectable extends Ability {
+export abstract class Selectable extends SelectablePrivate {
   /**
-   * @description Privately stored selected state.
-   * @type {Selected}
-   */
-  #selected = new Selected();
-
-  /**
-   * @description Sets the selectable state to unselected if the ability is enabled.
+   * @description Returns the `string` tag representation of the `Selectable` class when used in `Object.prototype.toString.call(instance)`.
    * @public
-   * @returns {this}
+   * @readonly
+   * @type {string}
    */
-  public deselect(): this {
-    super.isEnabled() && this.#selected.false();
-    return this;
-  }
-
-  /**
-   * @description Checks whether the selectable state is selected.
-   * @public
-   * @returns {boolean}
-   */
-  public isSelected(): boolean {
-    return this.#selected.is();
-  }
-
-  /**
-   * @description Sets the selectable state to selected if the ability is enabled.
-   * @public
-   * @returns {this}
-   */
-  public select(): this {
-    super.isEnabled() && this.#selected.true();
-    return this;
-  }
-
-  /**
-   * @description Toggles the selectable state between selected and unselected if the ability is enabled.
-   * @public
-   * @returns {this}
-   */
-  public toggle(): this {
-    super.isEnabled() && this.#selected.toggle();
-    return this;
+  public override get [Symbol.toStringTag](): string {
+    return Selectable.name;
   }
 }
