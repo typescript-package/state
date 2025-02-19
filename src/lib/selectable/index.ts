e7815cb99af5f8c44888e7f665e28d2ac460a162
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-02-19 11:51:15 +0000

    chore(selectable): update. index.

--- src/lib/selectable/index.ts
+++ src/lib/selectable/index.ts
@@ -1,2 +1,4 @@
 export { Selectable } from './selectable.abstract';
 export { SelectableGroup } from './selectable-group.abstract';
+export { SelectablePrivate } from './selectable-private.abstract';
+export { SelectableWeakMap } from './selectable-weak-map.abstract';
