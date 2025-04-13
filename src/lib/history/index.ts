ba3db4d66bda12ffc385130e72e872dcbfd21ed4
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-04-13 02:50:45 +0000

    refactor(history): remove in favor of `@typescript-package/history`.

--- src/lib/history/index.ts
+++
@@ -1,4 +0,0 @@
-export { HistoryAppend } from './history-append.abstract';
-export { HistoryCore } from './history-core.abstract';
-export { HistoryPrepend } from './history-prepend.abstract';
-export { History } from './history.abstract';
