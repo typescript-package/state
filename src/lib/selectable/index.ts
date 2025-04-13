2b9b6fa00cfac2fddbd67d3de342ef1498184ef4
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-04-13 02:49:19 +0000

    chore(selectable): move to the behavior.

--- src/lib/selectable/index.ts
+++
@@ -1,4 +0,0 @@
-export { Selectable } from './selectable.abstract';
-export { SelectableGroup } from './selectable-group.abstract';
-export { SelectablePrivate } from './selectable-private.abstract';
-export { SelectableWeakMap } from './selectable-weak-map.abstract';
