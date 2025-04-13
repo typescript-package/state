e156e164a4a1f78be26fdd6119c259c70bf3e207
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-04-13 13:13:27 +0000

    refactor(State): add `StateHistory` and ability to use different data storage.

--- src/lib/state.abstract.ts
+++ src/lib/state.abstract.ts
@@ -1,74 +1,107 @@
 // Abstract.
-import { StateImmutable } from "./state-immutable.abstract";
+import { Data, DataCore } from "@typescript-package/data";
+import { StateStorage } from "./state-storage.abstract";
+// Class.
+import { StateHistory } from "./state-history.class";
+// Type.
+import { DataConstructor, DataConstructors } from "./type";
 /**
- * @description Common `abstract class` for setting the state of the generic type variable `Type`.
- * 1. **Memory Efficiency**: Uses weak references to allow for garbage collection of state when the object is no longer in use.
- * 2. **Enhanced Encapsulation**: State is stored separately from the object, ensuring it is only accessible through the provided methods.
- * 3. **Flexible State Management**: Enables dynamic and isolated state handling, allowing for better extensibility and control over state across instances. * @export
+ * @description Common `abstract class` for setting the state of the generic type variable `Value`.
+ * @export
  * @abstract
  * @class State
- * @template Type
- * @extends {StateImmutable}
+ * @template Value 
+ * @template {DataCore<Value>} [DataType=Data<Value>] 
+ * @extends {StateStorage<Value, DataType>}
  */
-export abstract class State<Type> extends StateImmutable {
+export abstract class State<
+  Value,
+  DataType extends DataCore<Value> = Data<Value>,
+  HistoryData extends DataCore<Value[]> = Data<Value[]>,
+> extends StateStorage<Value, DataType> {
   /**
-   * @description Privately stored state of `Type`.
-   * @static
-   * @readonly
-   * @type {WeakMap}
-   */
-  static readonly #state = new WeakMap<State<any>, any>();
-
-  /**
-   * @description Returns the current state of `Type`.
+   * @description Returns the `string` tag representation of the `State` class when used in `Object.prototype.toString.call(instance)`.
    * @public
    * @readonly
-   * @type {Type}
+   * @type {string}
    */
-  public get state(): Readonly<Type> {
-    return State.#state.get(this);
+  public override get [Symbol.toStringTag](): string {
+    return State.name;
   }
 
   /**
-   * Creates an instance of child class.
+   * @description The `StateHistory` object that is used to store the history of the state.
+   * @public
+   * @type {StateHistory<Value, number>}
+   */
+  public get history() {
+    return this.#history;
+  }
+
+  /**
+   * @description Returns the current `Readonly` state of `Value`.
+   * @public
+   * @readonly
+   * @type {Readonly<Value>}
+   */
+  public get value() {
+    return this.state;
+  }
+
+  /**
+   * @description The `StateHistory` object that is used to store the history of the state.
+   * @type {?StateHistory<Value, number, HistoryData>}
+   */
+  #history?: StateHistory<Value, number, HistoryData>;
+
+  /**
+   * Creates an instance of `State`.
    * @constructor
-   * @param {Type} initialState Initial state of `Type`.
+   * @param {Value} state 
+   * @param {?DataConstructors<Value, [DataType, HistoryData]>} [data] 
    */
-  constructor(initialState: Type) {
-    super();
-    State.#state.set(this, initialState)
+  constructor(
+    state: Value,
+    track: number = 0,
+    data?: DataConstructors<Value, [DataType, HistoryData]>
+  ) {
+    super(state, Array.isArray(data) ? data[0] : Data as unknown as DataConstructor<Value, DataType>);
+    typeof track === 'number' && track > 0 && (this.#history = new StateHistory(
+      {value: state, size: track},
+      Array.isArray(data) ? data[1] : Data as unknown as DataConstructor<Value[], HistoryData>
+    ));
+    this.#history?.onRedo((value: Value) => this.set(value));
+    this.#history?.onUndo((value: Value) => this.set(value));
   }
-  
+
   /**
-   * @description Performs the `callback` function on `state`.
+   * @description Clears the state by removing all stored values.
    * @public
-   * @param {(state: Type) => void} stateCallback The callback function with a `state` to perform.
-   * @returns {this}
+   * @returns {this} 
    */
-  public on(stateCallback: (state: Type) => void): this {
-    stateCallback(State.#state.get(this));
+  public clear(): this {
     return this;
   }
 
   /**
-   * @description Removes the state from `WeakMap`.
+   * @description Destroys the state by removing all stored values and references.
+   * This method is typically used to clean up resources when the state is no longer needed.
    * @public
+   * @returns {this} 
    */
-  public destroy() {
-    State.#state.delete(this);
+  public destroy(): this {
+    return this;
   }
 
   /**
-   * @description Sets the state if the object is not locked and is allowed.
+   * @inheritdoc
    * @public
-   * @param {Type} state The state of `Type` to set.
-   * @returns {this}
+   * @param {Value} value 
+   * @returns {this} 
    */
-  protected set(state: Type): this {
-    if (super.isLocked()) {
-      throw new Error('Cannot set when object is locked.');
-    }
-    State.#state.set(this, state);
+  public override set(value: Value) {
+    super.set(value);
+    this.#history?.set(value);
     return this;
   }
 }
