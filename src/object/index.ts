0d08eec46b6f004a5ed0864bb432608258a643f4
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-04-27 11:53:03 +0000

    chore(object): reorganize.

--- src/object/index.ts
+++ src/object/index.ts
@@ -1,4 +1,1 @@
-export { BooleanActionObjectState } from './boolean-action-object-state.abstract';
-export { BooleanObjectState } from './boolean-object-state.abstract';
-export { NamedObjectState } from './named-object-state.abstract';
-export { ObjectState } from './object-state.abstract';
+export * from './lib';
