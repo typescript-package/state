209a3630e54e0cd3e54f0ab63f851ae84920f14c
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-08 22:35:24 +0000

    refactor(`State`): move the base logic to the `StateBase` and preserve the simple `data` only.

--- src/lib/state.abstract.ts
+++ src/lib/state.abstract.ts
@@ -1,25 +1,38 @@
 // Abstract.
 import { Data, DataCore } from "@typescript-package/data";
-import { StateStorage } from "./state-storage.abstract";
+import { StateBase } from "./state-base.abstract";
 // Class.
-import { StateHistory } from "./state-history.class";
+import { CurrentHistory, RedoHistory, UndoHistory } from "@typescript-package/history";
 // Type.
-import { DataConstructor, DataConstructors } from './type';
+import { DataConstructors } from "./type";
 /**
  * @description Common `abstract class` for setting the state of the generic type variable `Value`.
  * @export
  * @abstract
  * @class State
  * @template Value 
+ * @template {number} Size 
  * @template {DataCore<Value>} [DataType=Data<Value>] 
  * @template {DataCore<readonly Value[]>} [HistoryData=Data<readonly Value[]>] 
- * @extends {StateStorage<Value, DataType>}
+ * @extends {StateBase<Value, Size, DataType, HistoryData, CurrentHistory<Value, HistoryData>,
+ *  RedoHistory<Value, Size, HistoryData>,
+ *  UndoHistory<Value, Size, HistoryData>>
+ * }
  */
 export abstract class State<
   Value,
+  Size extends number,
   DataType extends DataCore<Value> = Data<Value>,
   HistoryData extends DataCore<readonly Value[]> = Data<readonly Value[]>,
-> extends StateStorage<Value, DataType> {
+> extends StateBase<
+  Value,
+  Size,
+  DataType,
+  HistoryData,
+  CurrentHistory<Value, HistoryData>,
+  RedoHistory<Value, Size, HistoryData>,
+  UndoHistory<Value, Size, HistoryData>
+> {
   /**
    * @description Returns the `string` tag representation of the `State` class when used in `Object.prototype.toString.call(instance)`.
    * @public
@@ -31,31 +44,6 @@ export abstract class State<
   }
 
   /**
-   * @description The `StateHistory` object that is used to store the history of the state.
-   * @public
-   * @type {StateHistory<Value, number>}
-   */
-  public get history() {
-    return this.#history;
-  }
-
-  /**
-   * @description Returns the current `Readonly` state of `Value`.
-   * @public
-   * @readonly
-   * @type {Value}
-   */
-  public get value(): Value {
-    return this.state;
-  }
-
-  /**
-   * @description The `StateHistory` object that is used to store the history of the state.
-   * @type {?StateHistory<Value, number, HistoryData>}
-   */
-  #history?: StateHistory<Value, number, HistoryData>;
-
-  /**
    * Creates an instance of `State` child class.
    * @constructor
    * @param {Value} value The initial value of `Value`.
@@ -65,49 +53,8 @@ export abstract class State<
   constructor(
     value: Value,
     track: number = 0,
-    data?: DataConstructors<Value, [DataType, HistoryData]>
+    data?: DataConstructors<Value, [DataType, HistoryData]>,
   ) {
-    super(value, Array.isArray(data) ? data[0] : Data as unknown as DataConstructor<Value, DataType>);
-    typeof track === 'number' && track > 0 && (this.#history = new StateHistory(
-      {value: value, size: track},
-      Array.isArray(data) ? data[1] : Data as unknown as DataConstructor<Value[], HistoryData>
-    ));
-    this.#history?.onRedo((value: Value) => super.set(value));
-    this.#history?.onUndo((value: Value) => super.set(value));
-  }
-
-  /**
-   * @description Clears the state by removing all stored values.
-   * @public
-   * @returns {this} The current instance.
-   */
-  public clear(): this {
-    super.data.clear();
-    this.#history?.clear();
-    return this;
-  }
-
-  /**
-   * @description Destroys the state by removing all stored values and references.
-   * This method is typically used to clean up resources when the state is no longer needed.
-   * @public
-   * @returns {this} The current instance.
-   */
-  public destroy(): this {
-    super.data.destroy();
-    this.#history?.destroy();
-    return this;
-  }
-
-  /**
-   * @inheritdoc
-   * @public
-   * @param {Value} value The value to set.
-   * @returns {this} The current instance.
-   */
-  public override set(value: Value) {
-    super.set(value);
-    this.#history?.set(value);
-    return this;
+    super(value, track, data, { current: CurrentHistory, redo: RedoHistory, undo: UndoHistory });
   }
 }
