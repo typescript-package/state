cd304d6732dfc9bc57d5a2a910002629c69b6872
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-05-08 22:32:24 +0000

    refactor(`StateHistory`): add current, redo and undo complete customization.

--- src/lib/state-history.class.ts
+++ src/lib/state-history.class.ts
@@ -1,28 +1,48 @@
 // Data.
 import { Data, DataCore } from "@typescript-package/data";
 // History.
-import { HistoryBase } from "@typescript-package/history";
+import {
+  CurrentHistory,
+  History,
+  HistoryCore,
+  HistoryCurrent,
+  RedoHistory,
+  UndoHistory,
+} from "@typescript-package/history";
 /**
  * @description The class to handle the history of the state.
  * @export
  * @class StateHistory
  * @template Value 
  * @template {number} [Size=number] 
- * @template {DataCore<Readonly<Value>[]>} [DataType=Data<Readonly<Value>[]>] 
- * @extends {HistoryBase<Readonly<Value>, Size, DataType>}
+ * @template {DataCore<readonly Value[]>} [DataType=Data<readonly Value[]>] 
+ * @template {HistoryCurrent<Value, DataType>} [CurrentType=CurrentHistory<Value, DataType>] 
+ * @template {HistoryCore<Value, Size, DataType>} [RedoType=RedoHistory<Value, Size, DataType>] 
+ * @template {HistoryCore<Value, Size, DataType>} [UndoType=UndoHistory<Value, Size, DataType>] 
+ * @extends {History<Value, Size, DataType, CurrentType, RedoType, UndoType>}
  */
 export class StateHistory<
   Value,
   Size extends number = number,
-  DataType extends DataCore<Readonly<Value>[]> = Data<Readonly<Value>[]>
-> extends HistoryBase<Readonly<Value>, Size, DataType> {
+  DataType extends DataCore<readonly Value[]> = Data<readonly Value[]>,
+  CurrentType extends HistoryCurrent<Value, DataType> = CurrentHistory<Value, DataType>,
+  RedoType extends HistoryCore<Value, Size, DataType> = RedoHistory<Value, Size, DataType>,
+  UndoType extends HistoryCore<Value, Size, DataType> = UndoHistory<Value, Size, DataType>
+> extends History<
+  Value,
+  Size,
+  DataType,
+  CurrentType,
+  RedoType,
+  UndoType
+> {
   /**
    * @description The default size of the history.
    * @public
    * @static
    * @type {number}
    */
-  public static track = 0;
+  public static track: number = 0;
 
   /**
    * @description Returns the `string` tag representation of the `StateHistory` class when used in `Object.prototype.toString.call(instance)`.
