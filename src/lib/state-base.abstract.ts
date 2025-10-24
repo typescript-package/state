21ca83991f5a41618df917913384fedea2cf88bc
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-10-24 15:23:48 +0000

    refactor(`StateBase`): add `subscribe()`, modify `clear()` `destroy()` methods by adding `history` param, track as `Size` generic, use `Listeners`.

--- src/lib/state-base.abstract.ts
+++ src/lib/state-base.abstract.ts
@@ -11,9 +11,10 @@ import {
 } from "@typescript-package/history";
 import { StateStorage } from "./state-storage.abstract";
 // Class.
+import { Listeners } from '@typescript-package/events';
 import { StateHistory } from "./state-history.class";
 // Type.
-import { DataConstructor, DataConstructors } from './type';
+import { DataConstructor, DataConstructors } from '../type';
 /**
  * @description Common `abstract class` for setting the state of the generic type variable `Value`.
  * @export
@@ -29,12 +30,19 @@ import { DataConstructor, DataConstructors } from './type';
  * @extends {StateStorage<Value, DataType>}
  */
 export abstract class StateBase<
+  // The type of state value.
   Value,
+  // History size.
   Size extends number,
+  // The type of state data holder.
   DataType extends DataCore<Value> = Data<Value>,
+  // The type of history data holder.
   HistoryData extends DataCore<readonly Value[]> = Data<readonly Value[]>,
+  // The type of current history.
   CurrentType extends HistoryCurrent<Value, HistoryData> = CurrentHistory<Value, HistoryData>,
+  // The type of redo history.
   RedoType extends HistoryCore<Value, Size, HistoryData> = RedoHistory<Value, Size, HistoryData>,
+  // The type of undo history.
   UndoType extends HistoryCore<Value, Size, HistoryData> = UndoHistory<Value, Size, HistoryData>,
 > extends StateStorage<Value, DataType> {
   /**
@@ -50,7 +58,7 @@ export abstract class StateBase<
   /**
    * @description The `StateHistory` object that is used to store the history of the state.
    * @public
-   * @type {StateHistory<Value, number>}
+   * @type {StateHistory<Value, Size, HistoryData, CurrentType, RedoType, UndoType> | undefined}
    */
   public get history() {
     return this.#history;
@@ -73,6 +81,12 @@ export abstract class StateBase<
   #history?: StateHistory<Value, Size, HistoryData, CurrentType, RedoType, UndoType>;
 
   /**
+   * @description
+   * @type {*}
+   */
+  #listeners = new Listeners<(value: Value) => void>();
+
+  /**
    * Creates an instance of `StateBase` child class.
    * @constructor
    * @param {Value} value The initial value of `Value`.
@@ -86,45 +100,50 @@ export abstract class StateBase<
    */
   constructor(
     value: Value,
-    track: number = 0,
+    track: Size = 0 as Size,
     data?: DataConstructors<Value, [DataType, HistoryData]>,
-    history: {
+    { current, redo, undo }: {
       current?: HistoryCurrentConstructor<Value, HistoryData, CurrentType>,
       redo?: HistoryCoreConstructor<Value, Size, HistoryData, RedoType>,
       undo?: HistoryCoreConstructor<Value, Size, HistoryData, UndoType>,
     } = {},
   ) {
     super(value, Array.isArray(data) ? data[0] : Data as unknown as DataConstructor<Value, DataType>);
+
+    // Initialize history if track is greater than 0.
     typeof track === 'number' && track > 0 && (this.#history = new StateHistory<Value, Size, HistoryData, CurrentType, RedoType, UndoType>(
-      {value: value, size: track as Size},
+      {value: value, size: track},
       Array.isArray(data) ? data[1] : Data as unknown as DataConstructor<Value[], HistoryData>,
-      history
+      { current, redo, undo }
     ));
     this.#history?.onRedo((value: Value) => super.set(value));
     this.#history?.onUndo((value: Value) => super.set(value));
   }
-
+  
   /**
    * @description Clears the state by removing all stored values.
    * @public
-   * @returns {this} The current instance.
+   * @param {boolean} [history=false] Whether to clean the history.
+   * @returns {this} 
    */
-  public clear(): this {
-    super.data.clear();
-    this.#history?.clear();
-    return this;
+  public clear(history = false): this {
+    return super.data.clear(),
+      history && this.#history?.clear(),
+      this.#listeners.clear(),
+      this;
   }
 
   /**
    * @description Destroys the state by removing all stored values and references.
    * This method is typically used to clean up resources when the state is no longer needed.
    * @public
-   * @returns {this} The current instance.
+   * @param {boolean} [history=true] Whether to destroy the history.
+   * @returns {this} 
    */
-  public destroy(): this {
-    super.data.destroy();
-    this.#history?.destroy();
-    return this;
+  public destroy(history = true): this {
+    return super.data.destroy(),
+      history && this.#history?.destroy(),
+      this;
   }
 
   /**
@@ -134,9 +153,26 @@ export abstract class StateBase<
    * @returns {this} The current instance.
    */
   public override set(value: Value) {
-    super.set(value);
-    this.#history?.set(value);
-    return this;
+    return super.set(value),
+      this.#history?.set(value),
+      this.#listeners.emit(value),
+      this;
+  }
+
+  /**
+   * Subscribe to state changes.
+   * @public
+   * @param {(value: Value) => void} listener Callback to run when state changes.
+   * @param {{ once: boolean }} [param0={ once: false }] 
+   * @param {boolean} param0.once Whether to subscribe once.
+   * @returns {() => void} Unsubscribe function.
+   */
+  public subscribe(
+    listener: (value: Value) => void,
+    { once }: { once: boolean } = { once: false }
+  ): () => void {
+    return this.#listeners.add(listener, { once }),
+      listener(this.value),
+      () => this.#listeners.delete(listener);
   }
 }
-
